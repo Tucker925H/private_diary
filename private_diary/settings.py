8edd0c441e117e0cd2e8d7defa20d5550b262ddc
Takayuki Honda <takka925H@gmail.com>
2022-02-04 23:50:15 +0900

    問い合わせページのフォーム画面を作成する

--- private_diary/settings.py
+++ private_diary/settings.py
@@ -3,7 +3,7 @@ from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-
+# BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
@@ -157,3 +157,7 @@ LOGGING = {
         },
     }
 }
+
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR,'static'),
+)
